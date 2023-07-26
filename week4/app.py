4ed00babccd35758b5a56f992a6469b53bb0e8b4
kvchgtw <138418959+kvchgtw@users.noreply.github.com>
2023-07-27 01:44:00 +0800

    submit

--- week4/app.py
+++ week4/app.py
@@ -1,6 +1,4 @@
 from flask import *
-from werkzeug.serving import *
-import threading
 
 
 app=Flask(
@@ -19,17 +17,36 @@ def index(): #用來回應網站首頁連線的函式
 #啟動網站伺服器可透過 port 指定埠號
 @app.route("/member")
 def member():
-    return render_template("member.html")
+    if "username" in session and session["username"] == "test":
+        print(session["username"])
+        return render_template("member.html")
+    else:  
+        return redirect("/")
+        
+    
 
-@app.route("/error/<errormsg>")
+#/error?msg=錯誤訊息
+@app.route("/error")
 def error():
-    return render_template("error.html")
+    message = request.args.get("msg", "發生錯誤，請聯繫客服 ")
+    return render_template("error.html", message=message)
 
-#動態路由
-@app.route("/user/<username>")
-def handleuser(username):
-    return "Hello" + username 
+@app.route("/signin", methods=["POST"])
+def signin():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "test" and password == "test":
+        session["username"] = "test"
+        return redirect("/member")
+    elif username =="" or password == "":
+        return redirect("/error?msg=請輸入帳號密碼")
+    else:
+        return redirect("/error?msg=帳號或密碼不正確")
 
+@app.route("/signout")
+def signout():
+    del session["username"]
+    return redirect("/")
 
 
 app.run(port=3000) 
